4c6a0739baf9a53bc896db2029a2e48557cc2478
gopipalani-dev <gopipalani.dev@gmail.com>
2023-09-12 15:00:46 +0530

    created t4,dart

---
+++ t4.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("hello world this is my first program upload in my git hub throough VSC");
+ { int a = 12;
+   int   b = 09;
+   int c = 2023;
+   int d = a+b+c;
+   print(d);  
+ }
+}
